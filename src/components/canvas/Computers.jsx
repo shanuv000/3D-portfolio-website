dc1181cdb8c2ebca78016e1cb6f701a77954340c
VAIBHAV KUMAR <48181718+shanuv000@users.noreply.github.com>
2023-04-25 13:47:05 +0530

    glb pro

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,8 +5,9 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
-  // const computer = useGLTF("./scene.glb");
+  // const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./desktop_pc/scene1.gltf");
+  // const computer = useGLTF("./scene2.glb");
 
   return (
     <mesh>
